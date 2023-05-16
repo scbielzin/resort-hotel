cd06c016bba5ee09c274295ab1803def57ea0475
Gabriel Arthur <gabrielarthur4146@gmail.com>
2023-05-16 16:29:06 -0300

    Dia 2 - Configurando rotas e paginas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
 
+import {Home} from "./pages/Home";
+import {Rooms} from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import {Route, Switch} from "react-router-dom";
+
 function App() {
   return (
     <>
-    hello react
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/rooms/" component={Rooms}/>
+    <Route exact path="/single-room/" component={SingleRoom}/>
     </>
   );
 }
