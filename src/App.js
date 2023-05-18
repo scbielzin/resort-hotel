cee4928ae8ea042f26c5e75b366120f841a06da4
Gabriel Arthur <gabrielarthur4146@gmail.com>
2023-05-18 17:15:48 -0300

    Dia 4 - Navbar Responsivo

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import { Rooms } from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/rooms" element={<Rooms />} />
-      <Route exact path="/rooms/:slug" element={<SingleRoom />} />
-      <Route path="*" element={<Error/>}></Route>
-    </Routes>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/rooms" element={<Rooms />} />
+        <Route exact path="/rooms/:slug" element={<SingleRoom />} />
+        <Route path="*" element={<Error/>}></Route>
+      </Routes>
+    </>
   );
 }
 
